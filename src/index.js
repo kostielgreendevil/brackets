1b437c250c19505025aca027365ed65603830564
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2019-10-02 15:02:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let arrConcat = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arrConcat = arrConcat.concat(bracketsConfig[i]);
+    }
+
+    let bracketsSet = new Set();
+    for (let i = 0; i < arrConcat.length; i++) {
+        bracketsSet.add(arrConcat[i]);
+    }
+
+    let prevStrLength = 0;
+    while (prevStrLength !== str.length && str.length > 0) {
+        prevStrLength = str.length;
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let trueBrackets = '';
+            let brackets = bracketsConfig[i];
+            if (bracketsSet.has(brackets[0])) {
+                trueBrackets += brackets[0];
+            }
+            if (bracketsSet.has(brackets[1])) {
+                trueBrackets += brackets[1];
+            }
+            str = str.replace(trueBrackets, '');
+        }
+    }
+    return str.length === 0;
 }
